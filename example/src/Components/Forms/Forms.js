3fb0958bb77c627814c5825c1c7fc1508709fa60
Ashkan Pourghasem <ashkan.pourghasem@gmail.com>
2019-01-16 08:42:52 +0330

    feat(Forms): Implement a simple form design

--- example/src/Components/Forms/Forms.js
+++ example/src/Components/Forms/Forms.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react'
 import styles from './Forms.module.scss'
 
-import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 class Forms extends Component {
 
     render() {
         return (
-            <div className={styles.simpleForm}>
-                <TextField
-                    id="name"
-                    label="Name"
-                    margin="dense"
-                    variant="outlined"
-                />
-            </div>
+            <Grid container className={styles.simpleForm}>
+                <Grid item className={styles.center} xs={6}>
+                    <Paper className={styles.paper}>
+                        <div className={styles.head}>
+                            <span>Simple Form</span>
+                        </div>
+
+                        <div className={styles.body}>
+                            <TextField
+                                fullWidth
+                                required
+                                label="Username"
+                                margin="dense"
+                                variant="outlined"
+                            />
+                            <TextField
+                                fullWidth
+                                required
+                                label="Email"
+                                margin="dense"
+                                variant="outlined"
+                            />
+                            <TextField
+                                fullWidth
+                                required
+                                label="Password"
+                                margin="dense"
+                                variant="outlined"
+                            />
+                            <TextField
+                                fullWidth
+                                required
+                                label="Confirm Password"
+                                margin="dense"
+                                variant="outlined"
+                            />
+                        </div>
+
+                        <div className={styles.actions}>
+                            <Button variant="contained" color="secondary">Submit</Button>
+                            <Button variant="contained">Reset</Button>
+                        </div>
+                    </Paper>
+                </Grid>
+                <Grid item className={styles.center} xs={6}>
+
+                </Grid>
+
+            </Grid>
         )
     }
 }
