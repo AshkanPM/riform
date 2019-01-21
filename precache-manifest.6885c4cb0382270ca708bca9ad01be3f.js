e498f21d49b3d87591b8804d04b835888e1a039f
AshkanPM <ashkan.pourghasem@gmail.com>
2019-01-21 13:44:47 +0330

    Updates

---
+++ precache-manifest.6885c4cb0382270ca708bca9ad01be3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4831ec7865bc64711839",
+    "url": "/riform/static/css/main.db41960e.chunk.css"
+  },
+  {
+    "revision": "4831ec7865bc64711839",
+    "url": "/riform/static/js/main.4831ec78.chunk.js"
+  },
+  {
+    "revision": "c88ddfcf683ca3e4739a",
+    "url": "/riform/static/js/1.c88ddfcf.chunk.js"
+  },
+  {
+    "revision": "4b7d274edeeaa368c764",
+    "url": "/riform/static/js/runtime~main.4b7d274e.js"
+  },
+  {
+    "revision": "f79ce5e9dd7932bc18f0ff60c4a27447",
+    "url": "/riform/index.html"
+  }
+];
